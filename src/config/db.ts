765ab18fb71c309e2223a45bc09b6bb611abd763
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-19 23:19:12 -0600

    conectando la aplicacion de Express a una base de datos

---
+++ src/config/db.ts
@@ -0,0 +1,5 @@
+import { Sequelize } from 'sequelize'
+
+const db  = new Sequelize('postgres://rest_api_node_typescript_5cti_user:IocH8V09ONwQYl1w5mOHTED8zuNu6Xvt@dpg-cppr9c6ehbks73c79ekg-a.oregon-postgres.render.com/rest_api_node_typescript_5cti?ssl=true')
+
+export default db
