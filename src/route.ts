98ccdcaa169855bf70264fd0ce7e50fa31533cd3
DaniM0613 <wwh.rj.92@gmail.com>
2024-06-24 17:12:13 -0600

    Moviendo la validacion hacia el Router

--- src/route.ts
+++ src/route.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { body} from 'express-validator'
 import { createProduct } from './handers/product'
 
 const router = Router()
@@ -8,7 +9,17 @@ router.get('/', (req, res) => {
  
  })
  
- router.post('/', createProduct)
+ router.post('/', 
+   // Validacion
+     body ('name')
+         .notEmpty().withMessage('El nombre de Producto no puede ir vacio'),
+        
+     body ('price')
+         .isNumeric().withMessage('Valor no valido')
+         .notEmpty().withMessage('El precio no puede ir vacio')
+         .custom(value => value > 0).withMessage('Precio no valido'),
+       createProduct
+)
 
 
  router.put('/', (req, res) => {
